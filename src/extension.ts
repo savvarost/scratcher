978d6770129c7b840a1f6e60d2490cc7d3ab66ad
Ростунцев Савва Дмитриевич <rostuntsev@tochka.com>
2024-07-16 09:41:59 +0500

    document

--- src/extension.ts
+++ src/extension.ts
@@ -1,70 +1,216 @@
-import * as vscode from 'vscode';
-import * as path from 'path';
-import * as fs from 'fs';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import * as path from "path";
+import * as utils from "./utils";
+import { FiletypesManager } from "./filetypes.manager";
+import { window } from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-    const view = new DirectoryViewer(context);
-    vscode.window.registerTreeDataProvider('directoryViewer', view);
+let scratcherDirectory: string;
 
-    let disposable = vscode.commands.registerCommand('extension.showDirectory', () => {
-        vscode.window.showInformationMessage('Directory Viewer is now active!');
+async function createScratchFile(directoryViewer: DirectoryViewer, folder_name: string) {
+  /**
+   * Функция создания scratch-файла
+   */
+  var filetypesManager = new FiletypesManager();
+
+  const selectedType = await filetypesManager.selectFiletype();
+
+  if (selectedType) {
+    // Формируем имя файла на основе текущей даты и времени
+    const timestamp = new Date().toISOString().replace(/[-:]/g, "").replace("T", "-").slice(0, 15);
+    const fileName = `${timestamp}${selectedType.ext}`;
+
+    const fileNameNew = await window.showInputBox({
+      placeHolder: "Input file name",
+      value: fileName,
     });
 
-    context.subscriptions.push(disposable);
+    if (fileNameNew) {
+      // Создаем директорию проекта, если она не существует
+      const folderPath = path.join(utils.getScratcherDirectory(), folder_name);
+      utils.ensureDirectoryExists(folderPath);
+
+      const filePath = path.join(utils.getScratcherDirectory(), folder_name, fileNameNew);
+      fs.writeFileSync(filePath, "");
+
+      vscode.workspace.openTextDocument(filePath).then((doc) => {
+        vscode.window.showTextDocument(doc);
+      });
+
+      directoryViewer.refresh();
+    }
+  }
+}
+
+async function createProjectScratchFile(directoryViewer: DirectoryViewer) {
+  /**
+   * Функция создания scratch-файла в отдельной директории соответствующей названию проекта
+   */
+  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
+  if (!workspaceFolder) {
+    vscode.window.showErrorMessage("No workspace folder opened.");
+    return;
+  }
+
+  const projectName = path.basename(workspaceFolder.uri.fsPath);
+  await createScratchFile(directoryViewer, projectName);
+}
+
+async function createTotalScratchFile(directoryViewer: DirectoryViewer) {
+  /**
+   * Функция создания scratch-файла в отдельной общей директории total
+   */
+  await createScratchFile(directoryViewer, "total");
+}
+
+export function activate(context: vscode.ExtensionContext) {
+  scratcherDirectory = utils.getScratcherDirectory();
+  utils.ensureDirectoryExists(scratcherDirectory);
+
+  const directoryViewer = new DirectoryViewer(context, scratcherDirectory);
+  vscode.window.registerTreeDataProvider("directoryViewer", directoryViewer);
+
+  vscode.commands.registerCommand("directoryViewer.refreshEntry", () => directoryViewer.refresh());
+
+  vscode.commands.registerCommand("directoryViewer.openFile", (resource) => {
+    vscode.window.showTextDocument(resource);
+  });
+
+  context.subscriptions.push(
+    directoryViewer,
+    vscode.commands.registerCommand("scratcher.createProjectScratchFile", () => createProjectScratchFile(directoryViewer)),
+    vscode.commands.registerCommand("scratcher.createTotalScratchFile", () => createTotalScratchFile(directoryViewer)),
+    vscode.commands.registerCommand("extension.createFile", async (node: FileItem) => {
+      const uri = await vscode.window.showInputBox({
+        prompt: "Enter file name",
+      });
+      if (uri) {
+        const filePath = path.join(node.resourceUri.fsPath, uri);
+        fs.writeFileSync(filePath, "");
+        vscode.window.showInformationMessage(`Created file: ${filePath}`);
+        directoryViewer.refresh();
+      }
+    }),
+    vscode.commands.registerCommand("extension.deleteFile", async (node: FileItem) => {
+      const confirm = await vscode.window.showWarningMessage(
+        `Are you sure you want to delete ${node.resourceUri.fsPath}?`,
+        "Yes",
+        "No"
+      );
+      if (confirm === "Yes") {
+        fs.unlinkSync(node.resourceUri.fsPath);
+        vscode.window.showInformationMessage(`Deleted file: ${node.resourceUri.fsPath}`);
+        directoryViewer.refresh();
+      }
+    }),
+    vscode.commands.registerCommand("extension.renameFile", async (node: FileItem) => {
+      const uri = await vscode.window.showInputBox({
+        prompt: "Enter new file name",
+      });
+      if (uri) {
+        const newFilePath = path.join(path.dirname(node.resourceUri.fsPath), uri);
+        fs.renameSync(node.resourceUri.fsPath, newFilePath);
+        vscode.window.showInformationMessage(`Renamed file: ${node.resourceUri.fsPath} to ${newFilePath}`);
+        directoryViewer.refresh();
+      }
+    }),
+    vscode.commands.registerCommand("extension.moveFile", async (node: FileItem) => {
+      const uri = await vscode.window.showInputBox({
+        prompt: "Enter destination directory",
+      });
+      if (uri && fs.existsSync(uri)) {
+        const newFilePath = path.join(uri, path.basename(node.resourceUri.fsPath));
+        fs.renameSync(node.resourceUri.fsPath, newFilePath);
+        vscode.window.showInformationMessage(`Moved file: ${node.resourceUri.fsPath} to ${newFilePath}`);
+        directoryViewer.refresh();
+      }
+    }),
+    vscode.commands.registerCommand("extension.copyFile", async (node: FileItem) => {
+      const uri = await vscode.window.showInputBox({
+        prompt: "Enter destination directory",
+      });
+      if (uri && fs.existsSync(uri)) {
+        const newFilePath = path.join(uri, path.basename(node.resourceUri.fsPath));
+        fs.copyFileSync(node.resourceUri.fsPath, newFilePath);
+        vscode.window.showInformationMessage(`Copied file: ${node.resourceUri.fsPath} to ${newFilePath}`);
+        directoryViewer.refresh();
+      }
+    }),
+    vscode.commands.registerCommand("extension.cutFile", async (node: FileItem) => {
+      const uri = await vscode.window.showInputBox({
+        prompt: "Enter destination directory",
+      });
+      if (uri && fs.existsSync(uri)) {
+        const newFilePath = path.join(uri, path.basename(node.resourceUri.fsPath));
+        fs.renameSync(node.resourceUri.fsPath, newFilePath);
+        vscode.window.showInformationMessage(`Moved file: ${node.resourceUri.fsPath} to ${newFilePath}`);
+        directoryViewer.refresh();
+      }
+    })
+  );
 }
 
 class DirectoryViewer implements vscode.TreeDataProvider<FileItem> {
-    private _onDidChangeTreeData: vscode.EventEmitter<FileItem | undefined | null | void> = new vscode.EventEmitter<FileItem | undefined | null | void>();
-    readonly onDidChangeTreeData: vscode.Event<FileItem | undefined | null | void> = this._onDidChangeTreeData.event;
+  private _onDidChangeTreeData: vscode.EventEmitter<FileItem | null | undefined> = new vscode.EventEmitter<
+    FileItem | null | undefined
+  >();
+  readonly onDidChangeTreeData: vscode.Event<FileItem | null | undefined> = this._onDidChangeTreeData.event;
 
-    constructor(private context: vscode.ExtensionContext) {}
+  private rootPath: string;
 
-    getTreeItem(element: FileItem): vscode.TreeItem {
-        return element;
+  constructor(private context: vscode.ExtensionContext, rootPath: string) {
+    this.rootPath = rootPath;
+  }
+
+  refresh(): void {
+    this._onDidChangeTreeData.fire(undefined);
+  }
+
+  getTreeItem(element: FileItem): vscode.TreeItem {
+    return element;
+  }
+
+  getChildren(element?: FileItem): Thenable<FileItem[]> {
+    if (element) {
+      return Promise.resolve(this.getFilesAndDirectories(element.resourceUri.fsPath));
+    } else {
+      return Promise.resolve(this.getFilesAndDirectories(this.rootPath));
     }
+  }
 
-    getChildren(element?: FileItem): Thenable<FileItem[]> {
-        if (element) {
-            return Promise.resolve(this.getFilesAndDirectories(element.resourceUri.fsPath));
-        } else {
-            const workspaceFolders = vscode.workspace.workspaceFolders;
-            if (workspaceFolders) {
-                return Promise.resolve(this.getFilesAndDirectories(workspaceFolders[0].uri.fsPath));
-            } else {
-                vscode.window.showInformationMessage('No folder or workspace opened');
-                return Promise.resolve([]);
-            }
-        }
-    }
+  private getFilesAndDirectories(dirPath: string): FileItem[] {
+    const files = fs.readdirSync(dirPath);
+    return files.map((file) => {
+      const filePath = path.join(dirPath, file);
+      const stat = fs.statSync(filePath);
+      return new FileItem(
+        vscode.Uri.file(filePath),
+        stat.isDirectory() ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None,
+        stat.isDirectory() ? "directory" : "file"
+      );
+    });
+  }
 
-    private getFilesAndDirectories(dirPath: string): FileItem[] {
-        const files = fs.readdirSync(dirPath);
-        return files.map(file => {
-            const filePath = path.join(dirPath, file);
-            const stat = fs.statSync(filePath);
-            return new FileItem(
-                vscode.Uri.file(filePath),
-                stat.isDirectory() ? vscode.TreeItemCollapsibleState.Collapsed : vscode.TreeItemCollapsibleState.None
-            );
-        });
-    }
+  dispose(): void {
+    // Cleanup resources
+    this._onDidChangeTreeData.dispose();
+  }
 }
 
 class FileItem extends vscode.TreeItem {
-    constructor(
-        public readonly resourceUri: vscode.Uri,
-        public readonly collapsibleState: vscode.TreeItemCollapsibleState
-    ) {
-        super(resourceUri, collapsibleState);
-        this.label = path.basename(resourceUri.fsPath);
-    }
-
-    iconPath = {
-        light: 'res/build.svg',
-        dark: 'res/build.svg',
+  constructor(
+    public readonly resourceUri: vscode.Uri,
+    public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+    public readonly contextValue: string
+  ) {
+    super(resourceUri, collapsibleState);
+    this.label = path.basename(this.resourceUri.fsPath);
+    this.command = {
+      command: "directoryViewer.openFile",
+      title: "Open File",
+      arguments: [this.resourceUri],
     };
-
-    contextValue = 'file';
+  }
 }
 
 export function deactivate() {}
